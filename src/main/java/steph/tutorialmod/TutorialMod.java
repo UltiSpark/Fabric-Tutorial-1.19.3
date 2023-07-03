f13c573add9f9ae5c5c8f408fe1283c1413301c0
UltiSpark <stephenpark3000@gmail.com>
2023-07-03 16:25:16 -0500

    Commit Test

--- src/main/java/steph/tutorialmod/TutorialMod.java
+++ src/main/java/steph/tutorialmod/TutorialMod.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//Very Important Comment
+//Very Important Comment 2
 public class TutorialMod implements ModInitializer {
 	public static final String Mod_ID = "tutorialmod";
     public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
